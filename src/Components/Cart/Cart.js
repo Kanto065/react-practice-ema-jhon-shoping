1d101c35b1f37d1aa66b84133931adddd01330a4
Kanta Bhattacharjee <kanto.uv@outlook.com>
2022-03-25 18:16:32 +0600

    localstorage save data

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+    const { cart } = props;
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const taxt = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + taxt;
     return (
-        <div>
+        <div className='cart'>
             <h4>order summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Shipping cost: ${shipping}</p>
+            <p>Tax: ${taxt}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
